83a351faa6673489f94447fc737a0a4689c24ce3
metas-rc <ruxandra.craciunescu@metasfresh.com>
2020-04-27 17:07:35 +0300

    #6520 invoice cand process - working
    https://github.com/metasfresh/metasfresh/issues/6520

--- de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/process/C_Invoice_Candidate_Recompute.java
+++ de.metas.swat/de.metas.swat.base/src/main/java/de/metas/invoicecandidate/process/C_Invoice_Candidate_Recompute.java
@@ -1,20 +1,22 @@
 package de.metas.invoicecandidate.process;
 
+import org.adempiere.ad.dao.IQueryBL;
+import org.adempiere.ad.dao.IQueryFilter;
+import org.compiere.model.IQuery;
+
+import de.metas.inoutcandidate.model.I_M_ShipmentSchedule;
+import de.metas.invoicecandidate.api.IInvoiceCandDAO;
+import de.metas.invoicecandidate.model.I_C_Invoice_Candidate;
 import de.metas.process.IProcessPrecondition;
 import de.metas.process.IProcessPreconditionsContext;
 import de.metas.process.JavaProcess;
 import de.metas.process.ProcessPreconditionsResolution;
+import de.metas.util.Services;
 import lombok.NonNull;
 
 public class C_Invoice_Candidate_Recompute extends JavaProcess implements IProcessPrecondition
 {
-
-	@Override
-	protected String doIt() throws Exception
-	{
-		// TODO Auto-generated method stub
-		return MSG_OK;
-	}
+	final IInvoiceCandDAO invCandDao = Services.get(IInvoiceCandDAO.class);
 
 	@Override
 	public ProcessPreconditionsResolution checkPreconditionsApplicable(@NonNull final IProcessPreconditionsContext context)
@@ -25,6 +27,20 @@ public class C_Invoice_Candidate_Recompute extends JavaProcess implements IProce
 		}
 
 		return ProcessPreconditionsResolution.accept();
+	}
+
+	@Override
+	protected String doIt() throws Exception
+	{
+		final IQueryFilter<I_C_Invoice_Candidate> queryFilterOrElseFalse = getProcessInfo().getQueryFilterOrElseFalse();
+
+		final IQuery<I_C_Invoice_Candidate> query = Services.get(IQueryBL.class).createQueryBuilder(I_C_Invoice_Candidate.class)
+				.filter(queryFilterOrElseFalse)
+				.create();
+
+		invCandDao.invalidateCandsFor(query);
+
+		return MSG_OK;
 	}
 
 }
