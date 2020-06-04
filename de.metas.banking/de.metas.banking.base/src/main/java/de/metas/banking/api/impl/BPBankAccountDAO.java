dec5d2af60636d2b6058d1b0bda484615339a570
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2020-06-04 20:07:09 +0200

    https://github.com/metasfresh/metasfresh/pull/6799
    manually porting the changes from master commit https://github.com/metasfresh/metasfresh/commit/58f0e4db77c2e77b16e75193adc7494872850558

--- de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/api/impl/BPBankAccountDAO.java
+++ de.metas.banking/de.metas.banking.base/src/main/java/de/metas/banking/api/impl/BPBankAccountDAO.java
@@ -60,7 +60,7 @@ public class BPBankAccountDAO implements IBPBankAccountDAO
 	}
 
 	@Override
-	public ImmutableListMultimap<BPartnerId, I_C_BP_BankAccount> getByBPartnerIds(@NonNull final Collection<BPartnerId> bpartnerIds)
+	public ImmutableListMultimap<BPartnerId, I_C_BP_BankAccount> getAllByBPartnerIds(@NonNull final Collection<BPartnerId> bpartnerIds)
 	{
 		if (bpartnerIds.isEmpty())
 		{
@@ -129,7 +129,7 @@ public class BPBankAccountDAO implements IBPBankAccountDAO
 	}
 
 	@Override
-	public void deactivateByBPartnerExcept(
+	public void deactivateIBANAccountsByBPartnerExcept(
 			@NonNull final BPartnerId bpartnerId,
 			@NonNull final Collection<BPartnerBankAccountId> exceptIds)
 	{
@@ -139,6 +139,7 @@ public class BPBankAccountDAO implements IBPBankAccountDAO
 
 		queryBL.createQueryBuilder(I_C_BP_BankAccount.class)
 				.addOnlyActiveRecordsFilter()
+				.addNotNull(I_C_BP_BankAccount.COLUMNNAME_IBAN)
 				.addEqualsFilter(I_C_BP_BankAccount.COLUMNNAME_C_BPartner_ID, bpartnerId)
 				.addNotInArrayFilter(I_C_BP_BankAccount.COLUMN_C_BP_BankAccount_ID, exceptIds)
 				.create()
